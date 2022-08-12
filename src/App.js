7957944cafbcd7edb00aad0b0b478e83b1bd1f6e
Noor Earpy <nuearpy1997@gmail.com>
2022-08-12 18:26:36 -0400

    initial commit
    This is a very simple file uploader using React JS.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./styles.css";
+import { useFileUpload } from "use-file-upload";
+const App = () => {
+  const defaultSrc =
+    "https://www.pngkit.com/png/full/301-3012694_account-user-profile-avatar-comments-fa-user-circle.png";
+
+  const [files, selectFiles] = useFileUpload();
+
+  return (
+    <div id="app">
+      <img src={files?.source || defaultSrc} alt="preview" />
+      <button
+        onClick={() =>
+          selectFiles({ accept: "image/*" }, ({ name, size, source, file }) => {
+            console.log("Files Selected", { name, size, source, file });
+          })
+        }
+      >
+        Upload Avatar
+      </button>
+    </div>
+  );
+};
+export default App;
